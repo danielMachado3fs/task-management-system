eeb9c43bc93e52bd18e1f567b1272045fb59e78a
DanielMachado <danielmachado3fs@gmail.com>
2024-03-06 21:51:28 -0300

    feat: configura biome js e adiciona arquivos base do crud de task

---
+++ task-server/src/task/dto/task.dto.ts
@@ -0,0 +1,34 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from 'class-transformer';
+import { IsBoolean, IsDateString, IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { StatusTaskEnum } from 'src/common/types';
+
+export class CreateTaskDto {
+	@IsString()
+	@IsNotEmpty()
+	title: string;
+
+	@IsString()
+	@IsNotEmpty()
+	description: string;
+
+	@IsDateString()
+	@IsNotEmpty()
+	dueDate: Date;
+
+	@IsBoolean()
+	@IsOptional()
+	@Transform(({ value }) => (typeof value === 'string' ? (value.toLowerCase() === 'true' ? true : false) : value))
+	public: boolean;
+
+	@IsString()
+	@IsOptional()
+	@IsEnum(StatusTaskEnum)
+	status: StatusTaskEnum;
+}
+
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+	@IsNotEmpty()
+	@IsString()
+	_id: string;
+}
